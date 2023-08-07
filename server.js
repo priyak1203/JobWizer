5450e2e7bd926abe8a9f466a96201acda09d38bd
Priydarshini Karanth <kpriya1203@gmail.com>
2023-08-07 13:36:46 +0530

    basic post request setup

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 
+// middlewares
+app.use(express.json());
+
+// routes
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.post('/', (req, res) => {
+  console.log(req);
+  res.json({ message: 'message received', data: req.body });
 });
 
 const PORT = process.env.PORT || 3000;
