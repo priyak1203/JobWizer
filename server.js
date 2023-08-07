ccabdf3820653944ce5b043bfa5743a3aba6ab22
Priydarshini Karanth <kpriya1203@gmail.com>
2023-08-07 21:07:53 +0530

    not found and error middlewares

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.get('/api/v1/jobs/:id', (req, res) => {
   const { id } = req.params;
   const job = jobs.find((job) => job.id === id);
   if (!job) {
+    throw new Error(`no job with id ${id}`);
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
   res.status(201).json({ job });
@@ -93,6 +94,17 @@ app.delete('/api/v1/jobs/:id', (req, res) => {
   jobs = newJobs;
 
   res.status(200).json({ msg: 'job deleted' });
+});
+
+// Not Found middleware
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'route does not exist' });
+});
+
+// Error middleware
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(500).json({ msg: 'something went wrong' });
 });
 
 const PORT = process.env.PORT || 3000;
