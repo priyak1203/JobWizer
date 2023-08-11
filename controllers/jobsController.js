826c309bb98417210cce64b53fccbadbe502b3aa
Priydarshini Karanth <kpriya1203@gmail.com>
2023-08-11 15:18:09 +0530

    validating id params advanced setup

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,5 @@
 import { StatusCodes } from 'http-status-codes';
 import Job from '../models/jobModel.js';
-import { NotFoundError } from '../errors/customErrors.js';
 
 // GET ALL JOBS
 export const getAllJobs = async (req, res) => {
@@ -18,22 +17,15 @@ export const createJob = async (req, res) => {
 export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
-
-  if (!job) throw new NotFoundError(`no job with id ${id}`);
-
   res.status(StatusCodes.OK).json({ job });
 };
 
 // UPDATE JOB
 export const updateJob = async (req, res) => {
   const { id } = req.params;
-
   const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
     new: true,
   });
-
-  if (!updatedJob) throw new NotFoundError(`no job with id ${id}`);
-
   res.status(StatusCodes.OK).json({ msg: 'job updated', job: updatedJob });
 };
 
@@ -41,8 +33,5 @@ export const updateJob = async (req, res) => {
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
   const removedJob = await Job.findByIdAndDelete(id);
-
-  if (!removedJob) throw new NotFoundError(`no job with id ${id}`);
-
   res.status(StatusCodes.OK).json({ msg: 'job deleted', job: removedJob });
 };
