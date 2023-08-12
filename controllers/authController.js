f21973c12d685fb2671452b323e6512ace50da94
Priydarshini Karanth <kpriya1203@gmail.com>
2023-08-12 12:49:21 +0530

    first user as admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,8 @@ import User from '../models/userModel.js';
 
 // Register User
 export const register = async (req, res) => {
+  const isFirstUser = (await User.countDocuments()) === 0;
+  req.body.role = isFirstUser ? 'admin' : 'user';
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
