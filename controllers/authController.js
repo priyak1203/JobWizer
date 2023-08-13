702479c632be6fe0f0b8c8b02f6b033082a957ef
Priydarshini Karanth <kpriya1203@gmail.com>
2023-08-13 15:12:10 +0530

    login user basic logic setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/userModel.js';
-import { hashPassword } from '../utils/passwordUtils.js';
+import { comparePassword, hashPassword } from '../utils/passwordUtils.js';
+import { UnauthenticatedError } from '../errors/customErrors.js';
 
 // Register User
 export const register = async (req, res) => {
@@ -16,5 +17,16 @@ export const register = async (req, res) => {
 
 // Login User
 export const login = async (req, res) => {
+  // check if the user exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) throw new UnauthenticatedError('invalid credentials');
+
+  // check if the password is correct
+  const isPasswordCorrect = await comparePassword(
+    req.body.password,
+    user.password
+  );
+  if (!isPasswordCorrect) throw new UnauthenticatedError('invalid credentials');
+
   res.send('login user');
 };
