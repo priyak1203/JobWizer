dc0e830a97394eb65a56f73e8a785813f5a4af29
Priydarshini Karanth <kpriya1203@gmail.com>
2023-08-13 12:28:46 +0530

    hash password in register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,19 @@
 import { StatusCodes } from 'http-status-codes';
+import bcrypt from 'bcryptjs';
 import User from '../models/userModel.js';
 
 // Register User
 export const register = async (req, res) => {
-  const isFirstUser = (await User.countDocuments()) === 0;
-  req.body.role = isFirstUser ? 'admin' : 'user';
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? 'admin' : 'user';
+
+  // random value that is added to the password before hashing
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: 'user created' });
 };
 
 // Login User
