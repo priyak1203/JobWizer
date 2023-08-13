34713de4ff3c256c7064ec3c21ac5d36dbee07fb
Priydarshini Karanth <kpriya1203@gmail.com>
2023-08-13 20:54:59 +0530

    jwt basic setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/userModel.js';
 import { comparePassword, hashPassword } from '../utils/passwordUtils.js';
 import { UnauthenticatedError } from '../errors/customErrors.js';
+import { createJWT } from '../utils/tokenUtils.js';
 
 // Register User
 export const register = async (req, res) => {
@@ -26,5 +27,7 @@ export const login = async (req, res) => {
 
   if (!isValidUser) throw new UnauthenticatedError('invalid credentials');
 
-  res.send('login user');
+  const token = createJWT({ userId: user._id, role: user.role });
+
+  res.json({ token });
 };
