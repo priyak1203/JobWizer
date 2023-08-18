8703e7d0e4b87fd03e5e567732b2f8a2a8bb14d2
Priydarshini Karanth <kpriya1203@gmail.com>
2023-08-18 21:11:28 +0530

    jobs container structure and styles

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,4 +1,27 @@
+import Wrapper from '../assets/wrappers/JobsContainer';
+import { useAllJobsContext } from '../pages/AllJobs';
+import Job from './Job';
+
 const JobsContainer = () => {
-  return <div>JobsContainer</div>;
+  const { jobs } = useAllJobsContext();
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>no jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
+
 export default JobsContainer;
