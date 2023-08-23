c272aca219ca007fcd0156a34ba0775438b670e6
Priydarshini Karanth <kpriya1203@gmail.com>
2023-08-23 22:57:01 +0530

    search container structure and filtering jobs based on search params

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,4 +1,40 @@
+import { Form, Link } from 'react-router-dom';
+import { JOB_SORT_BY, JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { FormRow, FormRowSelect, SubmitBtn } from '.';
+
 const SearchContainer = () => {
-  return <div>SearchContainer</div>;
+  return (
+    <Wrapper>
+      <Form className="form">
+        <h5 className="form-title">search form</h5>
+        <div className="form-center">
+          <FormRow type="search" name="search" />
+          <FormRowSelect
+            name="jobStatus"
+            labelText="job status"
+            list={['all', ...Object.values(JOB_STATUS)]}
+            defaultValue="all"
+          />
+          <FormRowSelect
+            name="jobType"
+            labelText="job type"
+            list={['all', ...Object.values(JOB_TYPE)]}
+            defaultValue="all"
+          />
+          <FormRowSelect
+            name="sort"
+            list={[...Object.values(JOB_SORT_BY)]}
+            defaultValue="newest"
+          />
+          <Link to="/dashboard/all-jobs" className="btn form-btn">
+            reset search values
+          </Link>
+          <SubmitBtn formBtn />
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
+
 export default SearchContainer;
--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -4,12 +4,16 @@ import customFetch from '../utils/customFetch';
 import { useLoaderData } from 'react-router-dom';
 import { createContext, useContext } from 'react';
 
-export const loader = async () => {
+export const loader = async ({ request }) => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+
   // get all jobs
   try {
     const {
       data: { jobs },
-    } = await customFetch.get('/jobs');
+    } = await customFetch.get('/jobs', { params });
     return { jobs };
   } catch (error) {
     toast.error(error?.response?.data?.msg);
