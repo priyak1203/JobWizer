41576bad14d14beb86274cafca6962da1f6a0ea7
Priydarshini Karanth <kpriya1203@gmail.com>
2023-08-25 20:58:34 +0530

    build front end programmatically

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === 'development') {
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 // test routes
 app.get('/', (req, res) => {
@@ -60,7 +60,7 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 // Not found route middleware
