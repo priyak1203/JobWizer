6c7fd3c943a672a4be597b1867c9c06e5bcbccec
Priydarshini Karanth <kpriya1203@gmail.com>
2023-08-27 21:16:16 +0530

    react query setup in stats loader

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -3,35 +3,22 @@ import { ChartsContainer, StatsContainer } from '../components';
 import customFetch from '../utils/customFetch';
 import { useQuery } from '@tanstack/react-query';
 
-export const loader = async () => {
-  // const response = await customFetch.get('/jobs/stats');
-  // return response.data;
-  // try {
-  //   const response = await customFetch.get('/jobs/statss');
-  //   return response.data;
-  // } catch (error) {
-  //   return error;
-  // }
+const statsQuery = {
+  queryKey: ['stats'],
+  queryFn: async () => {
+    const response = await customFetch.get('/jobs/stats');
+    return response.data;
+  },
+};
 
+export const loader = (queryClient) => async () => {
+  await queryClient.ensureQueryData(statsQuery);
   return null;
 };
 
 const Stats = () => {
-  // const { defaultStats, monthlyApplications } = useLoaderData();
-
-  // traditional react query setup
-  const response = useQuery({
-    queryKey: ['stats'],
-    queryFn: () => customFetch.get('/jobs/stats'),
-  });
-
-  const { isLoading, isError, data } = response;
-
-  if (isLoading) return <h4>Loading....</h4>;
-
-  if (isError) return <h4>Error...</h4>;
-
-  const { monthlyApplications, defaultStats } = data.data;
+  const { data } = useQuery(statsQuery);
+  const { monthlyApplications, defaultStats } = data;
 
   return (
     <>
