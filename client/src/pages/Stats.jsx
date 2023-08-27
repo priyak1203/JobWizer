30e35c80543d8de50db10adead94e3e868fcacf7
Priydarshini Karanth <kpriya1203@gmail.com>
2023-08-27 20:48:37 +0530

    react query traditional setup in stats page

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,21 +1,37 @@
 import { useLoaderData } from 'react-router-dom';
 import { ChartsContainer, StatsContainer } from '../components';
 import customFetch from '../utils/customFetch';
+import { useQuery } from '@tanstack/react-query';
 
 export const loader = async () => {
-  const response = await customFetch.get('/jobs/statss');
-  return response.data;
-
+  // const response = await customFetch.get('/jobs/stats');
+  // return response.data;
   // try {
   //   const response = await customFetch.get('/jobs/statss');
   //   return response.data;
   // } catch (error) {
   //   return error;
   // }
+
+  return null;
 };
 
 const Stats = () => {
-  const { defaultStats, monthlyApplications } = useLoaderData();
+  // const { defaultStats, monthlyApplications } = useLoaderData();
+
+  // traditional react query setup
+  const response = useQuery({
+    queryKey: ['stats'],
+    queryFn: () => customFetch.get('/jobs/stats'),
+  });
+
+  const { isLoading, isError, data } = response;
+
+  if (isLoading) return <h4>Loading....</h4>;
+
+  if (isError) return <h4>Error...</h4>;
+
+  const { monthlyApplications, defaultStats } = data.data;
 
   return (
     <>
